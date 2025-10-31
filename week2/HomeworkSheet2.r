bf333cf90fab25c5f41746ebb56e2408ead8911c
Eduardo Venegas <lalohdez77@gmail.com>
2025-10-31 18:15:35 +0100

    Finish Q3

--- week2/HomeworkSheet2.r
+++ week2/HomeworkSheet2.r
@@ -12,8 +12,12 @@
 
 
 ## Please write below your (and your teammates) name, matriculation number. 
-## Name: Mahalakshmi, Elias, Eduardo
-## Matriculation number: 7071187, 7055205
+## Name: Mahalakshmi Raveenthiran
+## Matriculation number: 7071187
+## Name: Eduardo Venegas
+## Matriculation number: 7069924
+## Name: Elias
+## Matriculation number:7055205
 
 ## Only 1 member needs to submit! 
 
@@ -60,25 +64,79 @@ t.test(USAccDeaths[1:12], USAccDeaths[61:72], alternative="greater")
 
 ## a) What does a confidence interval mean from the perspective of experiment replication?
 
+# It means that if we were to repeat the experiment multiple times and calculate a confidence interval (for example, 95%), we would expect that approximately 95% of those intervals would contain the true population mean.
+
+# ____________________________________________________________________________
+
 ## b) please install and load packages sciplot and lsr
 
+install.packages("sciplot")
+install.packages("lsr")
+library("sciplot")
+library("lsr")
+
+# ____________________________________________________________________________
+
 ## c) We will be working with the dataset rivers, that lists the length of major US rivers. What does the following line do?
+
 sample15 = sample(rivers,15)
+# it randomly samples 15 river lengths
+
+# ____________________________________________________________________________
 
 ## d) calculate 95% Confidence Intervals for rivers and sample15. You can
 ##    use the function ciMean()
+ciMean(rivers, conf=.95)
+ciMean(sample15, conf=.95)
+
+# ____________________________________________________________________________
 
 ## e) Why are the intervals different?
+
+# they are different because a smaller sample size introduces more variability and uncertainty in estimating the population mean, this results in a wider confidence interval for sample15 compared to rivers. We go from 508-673 for rivers to 426-716 for sample15.
+
+# ____________________________________________________________________________
 
 ## f) We will now calculate the confidence interval for the mean of rivers by hand. 
 ##    A commonly used formula for 95% confidence intervals is mean±1.96*SE
 
+avg <- mean(rivers)
+se <- sd(rivers) / sqrt(length(rivers))
+lower_bound <- avg - 1.96 * se
+upper_bound <- avg + 1.96 * se
+c(lower_bound, upper_bound)
+
+# ____________________________________________________________________________
+
 ## g) Do your results align with the output of ciMean above? (There might be a bit of rounding error)
+
+# Yes, the results align with the output of ciMean above.
+# > c(lower_bound, upper_bound)
+# [1] 509.6652 672.7036
+
+# > ciMean(rivers, conf=.95)
+#            2.5%    97.5%
+# rivers 508.9559 673.4129
+
+# ____________________________________________________________________________
 
 ## h) Now, please calculate a 90% confidence interval without the ciMean function. 
 ##  Hint: You need to replace the 1.96, which is the 97.5 quantile of the normal distribution.
 
+z_score <- qnorm(0.95)
+lower_bound <- avg - z_score * se
+upper_bound <- avg + z_score * se
+c(lower_bound, upper_bound)
+# 522.7726 659.5962
+
+# > ciMean(rivers, conf=.90)
+#              5%      95%
+
+# ____________________________________________________________________________
+
 ## i) Compare to the interval in g!
+
+# The 90% confidence interval is narrower than the 95% confidence interval. We become less certain about the population mean as we decrease the confidence level, which results in a smaller interval.
 
 
 #################################################################################
@@ -103,7 +161,7 @@ library("forcats")
 
 ## b. read in the data
 getwd()
-insomnia <- read.csv("/home/elias/Documents/StatsWithR/statistics_r/week2/insomnia25.csv")
+insomnia <- read.csv("./week2/insomnia25.csv")
 
 ## c. get a summary of the dataset
 glimpse(insomnia)
@@ -112,11 +170,29 @@ glimpse(insomnia)
 ##    and 1 for sleep problems.
 ##    Make sure that this is the case
 insomnia %>%
-    filter(sleepProblem >= 0, sleepProblem <= 1)
+    count(sleepProblem)
+
+# insomnia$sleepProblem  n
+# 1                     0 39
+# 2                     1 22
+# 3                    11  1
+# 4                   yes  1
+
+insomnia <- insomnia %>%
+    mutate(sleepProblem = ifelse(sleepProblem == "yes", 1, sleepProblem)) %>%
+    mutate(sleepProblem = ifelse(sleepProblem == 11, 1, sleepProblem)) %>%
+    mutate(sleepProblem = as.numeric(sleepProblem))
+
+insomnia %>%
+    count(sleepProblem)
+#   sleepProblem  n
+# 1            0 39
+# 2            1 24
 
 ## e. how many students encounter sleep problems?
 insomnia %>%
-    count(insomnia$sleepProblem == 1)
+    filter(sleepProblem == 1) %>%
+    nrow()
 
 ## f. how many different drinks do students name? (transform the variable into a 
 ## factor first)
